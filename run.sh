73c0f791945b2baa248e6c91c1c14577eedef839
Ryan Burda <ryanburda@Ryans-MacBook-Pro.local>
2019-03-26 00:49:29 -0400

    Made the paths in run.sh relative.

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
-ln -sf ~/Developer/config/bashrc ~/.bashrc
-ln -sf ~/Developer/config/bash_profile ~/.bash_profile
-ln -sf ~/Developer/config/zshrc ~/.zshrc
-ln -sf ~/Developer/config/vimrc ~/.vimrc
-ln -sf ~/Developer/config/direnvrc ~/.direnvrc
-ln -sf ~/Developer/config/karabiner.json ~/.config/karabiner/karabiner.json
+# Assumes all of the config files are in the same directory as this script.
+DIRECTORY=$(pwd)
+ln -sf $DIRECTORY/bashrc ~/.bashrc
+ln -sf $DIRECTORY/bash_profile ~/.bash_profile
+ln -sf $DIRECTORY/zshrc ~/.zshrc
+ln -sf $DIRECTORY/vimrc ~/.vimrc
+ln -sf $DIRECTORY/direnvrc ~/.direnvrc
+ln -sf $DIRECTORY/karabiner.json ~/.config/karabiner/karabiner.json
