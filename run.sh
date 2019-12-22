ae1f629ed506d367cd528fbbfec28a31ff01328e
Ryan Burda <ryanburda@Ryans-MacBook-Pro.local>
2019-12-22 15:16:19 -0500

    Added zsh to setup script

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ DIRECTORY=$(pwd)
 ln -sf $DIRECTORY/bashrc ~/.bashrc
 ln -sf $DIRECTORY/bash_profile ~/.bash_profile
 ln -sf $DIRECTORY/zshrc ~/.zshrc
+ln -sf $DIRECTORY/zsh ~/.zsh
 ln -sf $DIRECTORY/vimrc ~/.vimrc
 ln -sf $DIRECTORY/direnvrc ~/.direnvrc
 ln -sf $DIRECTORY/karabiner.json ~/.config/karabiner/karabiner.json
