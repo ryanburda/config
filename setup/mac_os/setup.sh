38c300412697463e1b06fcdeeeea887b656b9e1b
Ryan Burda <ryan.burda@gmail.com>
2020-03-01 23:39:35 -0500

    Added keyboard shortcuts plist

--- setup/mac_os/setup.sh
+++ setup/mac_os/setup.sh
@@ -1,2 +1,7 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ln -sfv $DIR/com.apple.symbolichotkeys.plist ~/Library/Preferences/com.apple.symbolichotkeys.plist
+
 defaults write -g InitialKeyRepeat -int 5 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
