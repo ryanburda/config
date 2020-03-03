ec233c1c169175bbf28352775e04ac385262328c
Ryan Burda <ryan.burda@gmail.com>
2020-03-02 20:10:25 -0500

    Fixed key repeat issue

--- setup/mac_os/setup.sh
+++ setup/mac_os/setup.sh
@@ -3,5 +3,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ln -sfv $DIR/com.apple.symbolichotkeys.plist ~/Library/Preferences/com.apple.symbolichotkeys.plist
 
-defaults write -g InitialKeyRepeat -int 5 # normal minimum is 15 (225 ms)
-defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 2 # normal minimum is 2 (30 ms)
