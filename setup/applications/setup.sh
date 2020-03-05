65b77a74ed74b2babcc6e5b35f9b4d8c147f6a9b
Ryan Burda <ryan.burda@gmail.com>
2020-03-04 22:57:43 -0500

    Added intellij to applications

--- setup/applications/setup.sh
+++ setup/applications/setup.sh
@@ -5,10 +5,13 @@ brew cask install iterm2
 brew cask install spotify
 brew cask install firefox
 brew cask install 1password
+brew cask install 1password-cli
 brew cask install docker
 brew cask install slack
 brew cask install pycharm
 brew cask install pycharm-ce
+brew cask install intellij-idea
+brew cask install intellij-idea-ce
 brew cask install datagrip
 brew cask install thingsmacsandboxhelper
 brew cask install nordvpn
