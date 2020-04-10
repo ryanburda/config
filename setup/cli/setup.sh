2e2e5b170841a5721aa91e8ce8f4b66db50b19a7
Ryan Burda <ryan.burda@gmail.com>
2020-04-10 08:17:07 -0400

    Added vault cli

--- setup/cli/setup.sh
+++ setup/cli/setup.sh
@@ -26,5 +26,7 @@ brew install watch
 brew install fswatch
 brew install telnet
 brew install kubectl
+brew install vault
+vault -autocomplete-install
 
 echo "Follow this article to finish setting up iterm https://medium.com/@Clovis_app/configuration-of-a-beautiful-efficient-terminal-and-prompt-on-osx-in-7-minutes-827c29391961"
