b1a20f939f5df4ccf146d980fc5ecdf76a2c3b70
Ryan Burda <rburda@squarespace.com>
2022-05-13 23:52:51 -0400

    lsp rename keymap

--- src/nvim/nvim/lua/lsp/server_default.lua
+++ src/nvim/nvim/lua/lsp/server_default.lua
@@ -5,7 +5,7 @@ local on_attach = function(client, bufnr)
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>J', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>k', '<cmd>lua vim.lsp.buf.hover()<CR>'          , opts)
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>K', '<cmd>lua vim.lsp.buf.signature_help()<CR>' , opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>R', '<cmd>lua vim.lsp.buf.rename()<CR>'         , opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>'         , opts)
     vim.api.nvim_buf_set_keymap(bufnr, 'i', '<C-k>'    , '<cmd>lua vim.lsp.buf.signature_help()<CR>' , opts)
 
     -- show signature help using the ray-x/lsp_signature.nvim plugin
