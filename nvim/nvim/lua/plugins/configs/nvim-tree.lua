3b5db6f0032a2c328c1521852a0e06443edd9a77
Ryan Burda <rburda@squarespace.com>
2022-05-25 18:59:00 -0400

    nvim-tree indent markers

--- nvim/nvim/lua/plugins/configs/nvim-tree.lua
+++ nvim/nvim/lua/plugins/configs/nvim-tree.lua
@@ -6,14 +6,26 @@ function T.setup()
     vim.api.nvim_set_keymap('n', '<leader>aa', ':NvimTreeFocus<CR>', opts)
     vim.api.nvim_set_keymap('n', '<leader>ax', ':NvimTreeClose<CR>', opts)
 
-    vim.cmd("let g:nvim_tree_show_icons = {'git': 0, 'folders': 1, 'files': 1, 'folder_arrows': 1 }")
+    vim.cmd("let g:nvim_tree_show_icons = {'git': 0, 'folders': 1, 'files': 1, 'folder_arrows': 0 }")
+    vim.cmd("let g:nvim_tree_autoclose = 1")
 
     require('nvim-tree').setup({
+        hijack_unnamed_buffer_when_opening = true,
         update_cwd = false,
         hijack_cursor = true,
         update_focused_file = {
             enable = true,
             update_cwd = true,
+        },
+        renderer = {
+            indent_markers = {
+                enable = true,
+                icons = {
+                    corner = "└ ",
+                    edge = "│ ",
+                    none = "  ",
+                },
+            },
         },
     })
 
