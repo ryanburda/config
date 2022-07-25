e02d81b903c1a27e8705d08a32120f7d7ca40cb1
Ryan Burda <rburda@squarespace.com>
2022-07-25 13:44:37 -0400

    squash

--- nvim/nvim/lua/plugins/init.lua
+++ nvim/nvim/lua/plugins/init.lua
@@ -112,15 +112,15 @@ require("packer").startup(function(use)
 
     -- pretty list of diagnostics, lsp, quickfix list, location list, telescope results
     use {
-      "folke/trouble.nvim",
-      requires = "kyazdani42/nvim-web-devicons",
-      config = function()
-        require("trouble").setup {
-          -- your configuration comes here
-          -- or leave it empty to use the default settings
-          -- refer to the configuration section below
-        }
-      end
+        "folke/trouble.nvim",
+        requires = "kyazdani42/nvim-web-devicons",
+        config = function()
+            require("trouble").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
     }
 
     -- Tmux
