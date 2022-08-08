8eed0d6cb1d95897a14071dc5408abce090b7a14
Ryan Burda <ryan.burda@gmail.com>
2022-08-08 14:30:05 -0400

    rust lsp

---
+++ nvim/nvim/lua/lsp/servers/rust_analyzer.lua
@@ -0,0 +1,15 @@
+local T = {}
+T.default = require('lsp.server_default')
+T.config = T.default.config
+
+function T.on_attach(client, bufnr)
+    T.default.on_attach(client, bufnr)
+
+    -- TODO: add debugger.
+end
+
+T.config = {}
+T.config.on_attach = T.on_attach
+
+return T
+
