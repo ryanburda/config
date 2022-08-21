cf75e2d0ddc38e62bb6df18a9d5c64e8d5063561
Ryan Burda <ryan.burda@gmail.com>
2022-08-21 10:48:58 -0400

    squash

--- nvim/nvim/lua/plugins/configs/symbols-outline.lua
+++ nvim/nvim/lua/plugins/configs/symbols-outline.lua
@@ -3,11 +3,15 @@ local T = {}
 function T.setup()
 
     require("symbols-outline").setup({
-        width = 20
+        width = 20,
     })
 
     local opts = { noremap=true, silent=false}
     vim.api.nvim_set_keymap('n', '<leader>ss', ":SymbolsOutline<cr>", opts)
+
+    -- Make the background darker.
+    -- local augroup = vim.api.nvim_create_augroup("SymbolsOutline", { clear = true })
+    -- vim.api.nvim_create_autocmd("FileType", { pattern = "Outline", command = "highlight! link NormalNC Pmenu", group = augroup })
 
 end
 
