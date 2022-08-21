573d46679e8314e4633e9e3af98e562e8f2c2315
Ryan Burda <ryan.burda@gmail.com>
2022-08-20 21:17:02 -0400

    undercurl

--- nvim/nvim/lua/plugins/configs/spellsitter.lua
+++ nvim/nvim/lua/plugins/configs/spellsitter.lua
@@ -5,7 +5,7 @@ function T.setup()
     require('spellsitter').setup()
 
     local augroup = vim.api.nvim_create_augroup("SpellSitter", { clear = true })
-    vim.api.nvim_create_autocmd("VimEnter", { command = "highlight! SpellBad guisp=red gui=undercurl guifg=NONE guibg=NONE ctermfg=NONE ctermbg=NONE term=underline cterm=underline", group = augroup })
+    vim.api.nvim_create_autocmd("VimEnter", { command = "highlight! SpellBad guisp=red gui=undercurl guifg=NONE guibg=NONE ctermfg=NONE ctermbg=NONE term=undercurl cterm=undercurl", group = augroup })
 
 end
 
