d73c7446f7217125aac0c4fb15f269cb6bd48d16
Ryan Burda <ryan.burda@gmail.com>
2022-08-20 21:46:03 -0400

    Illuminate on VimEnter

--- nvim/nvim/lua/plugins/configs/vim-illuminate.lua
+++ nvim/nvim/lua/plugins/configs/vim-illuminate.lua
@@ -2,9 +2,9 @@ local T = {}
 
 function T.setup()
     local augroup = vim.api.nvim_create_augroup("VimIlluminate", { clear = true })
-    vim.api.nvim_create_autocmd("BufEnter", { command = "highlight! link IlluminatedWordText CursorLine", group = augroup })
-    vim.api.nvim_create_autocmd("BufEnter", { command = "highlight! link IlluminatedWordRead CursorLine", group = augroup })
-    vim.api.nvim_create_autocmd("BufEnter", { command = "highlight! link IlluminatedWordWrite CursorLine", group = augroup })
+    vim.api.nvim_create_autocmd("VimEnter", { command = "highlight! link IlluminatedWordText CursorLine", group = augroup })
+    vim.api.nvim_create_autocmd("VimEnter", { command = "highlight! link IlluminatedWordRead CursorLine", group = augroup })
+    vim.api.nvim_create_autocmd("VimEnter", { command = "highlight! link IlluminatedWordWrite CursorLine", group = augroup })
 
     require('illuminate').configure()
 end
