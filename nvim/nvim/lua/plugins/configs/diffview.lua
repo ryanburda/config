19e387064a02a2001fbe4b3e5d5b5e239f9cfae0
Ryan Burda <ryan.burda@gmail.com>
2022-08-23 00:21:51 -0400

    focus

--- nvim/nvim/lua/plugins/configs/diffview.lua
+++ nvim/nvim/lua/plugins/configs/diffview.lua
@@ -1,17 +1,32 @@
 local T = {}
 
 function T.setup()
+
     local opts = { noremap=true, silent=false }
-    -- Convention: lowercase mappings open diffview.
     vim.api.nvim_set_keymap('n', '<leader>dd', ':DiffviewOpen<cr>'         , opts)
     vim.api.nvim_set_keymap('n', '<leader>do', ':DiffviewOpen origin/'     , opts)
     vim.api.nvim_set_keymap('n', '<leader>dl', ':DiffviewOpen '            , opts)
-
     vim.api.nvim_set_keymap('n', '<leader>df', ':DiffviewFileHistory<cr>'  , opts)
     vim.api.nvim_set_keymap('n', '<leader>da', ':DiffviewFileHistory .<cr>', opts)
     vim.api.nvim_set_keymap('n', '<leader>dx', ':DiffviewClose<cr>'        , opts)
     vim.api.nvim_set_keymap('n', '<leader>dr', ':DiffviewRefresh<cr>'      , opts)
     vim.api.nvim_set_keymap('n', '<leader>dt', ':DiffviewToggleFiles<cr>'  , opts)
+
+    require("diffview").setup({
+        enhanced_diff_hl = true,
+        file_panel = {
+            listing_style = "tree",
+            tree_options = {
+                flatten_dirs = true,
+                folder_statuses = "only_folded",
+            },
+            win_config = {
+                position = "left",
+                width = 40,
+            },
+        },
+    })
+
 end
 
 return T
--- nvim/nvim/lua/plugins/configs/focus.lua
+++ nvim/nvim/lua/plugins/configs/focus.lua
@@ -3,15 +3,35 @@ local T = {}
 function T.setup()
 
     require("focus").setup({
+        enable = false,
         width = 120,
         hybridnumber = false,
         signcolumn = false,
         quickfixheight = 20,
-        treewidth = require('plugins.configs.nvim-tree').TREE_WIDTH,
-        compatible_filetrees = {"NvimTree"},
-        excluded_filetypes = { "toggleterm", "DiffviewFiles", "Outline", },
-        excluded_buftypes = { "nofile", "prompt", "popup", "help", "DiffviewFiles", }
+        compatible_filetrees = {"nvimtree", "FileTree", "nerdtree", "chadtree", "fern", },
+        treewidth = require("plugins.configs.nvim-tree").TREE_WIDTH,
+        excluded_filetypes = {
+            "toggleterm",
+            "DiffviewFiles",
+            "Outline",
+            "dapui_console",
+            "dap-repl",
+            "dapui_stacks",
+            "dapui_scopes",
+            "dapui_watches",
+        },
+        excluded_buftypes = {
+            "nofile",
+            "prompt",
+            "popup",
+            "help",
+        },
     })
+
+    local opts = { noremap=true, silent=false}
+    vim.api.nvim_set_keymap("n", "<leader>'", ":FocusEnable<cr>" , opts)
+    vim.api.nvim_set_keymap("n", "<leader>;", ":FocusDisable<cr>", opts)
+
 
 end
 
