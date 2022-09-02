de20d86aa7160184b7b683e32ebe0587b2615ce6
Ryan Burda <rburda@squarespace.com>
2022-09-02 18:24:03 -0400

    sql file type specific Vtr command

--- nvim/nvim/lua/plugins/configs/vim-tmux-runner.lua
+++ nvim/nvim/lua/plugins/configs/vim-tmux-runner.lua
@@ -7,24 +7,29 @@ function T.setup()
     vim.g.VtrAppendNewline = 1
 
     vim.cmd([[
-    function! VtrSendLinesToRunnerPSQL() range
-        " `q` exists the psql pager if it is up.
-        VtrSendKeysRaw q
-        " get rid of the `q` if the psql pager wasn't open.
-        VtrSendCtrlC
-        " clear the screen.
-        VtrSendCommandToRunner \! clear
-        " send the lines.
-        VtrSendLinesToRunner
+    function! VtrSendLinesToRunnerWrapper() range
+        if &filetype == 'sql'
+            " `q` exists the psql pager if it is up.
+            VtrSendKeysRaw q
+            " get rid of the `q` if the psql pager wasn't open.
+            VtrSendCtrlC
+            " clear the screen.
+            VtrSendCommandToRunner \! clear
+            " send the lines.
+            '<,'>VtrSendLinesToRunner
+        else
+            " send the lines.
+            '<,'>VtrSendLinesToRunner
+        endif
     endfunction
     ]])
 
     local opts = { noremap=true, silent=true }
 
-    vim.api.nvim_set_keymap("n", "<leader>ei", ":VtrOpenRunner<cr>"                  , opts)
-    vim.api.nvim_set_keymap("n", "<leader>ea", ":VtrAttachToPane<cr>"                , opts)
-    vim.api.nvim_set_keymap("x", "<leader>ee", ":call VtrSendLinesToRunnerPSQL()<cr>", opts)
-    vim.api.nvim_set_keymap("n", "<leader>ef", ":VtrSendFile<cr>"                    , opts)
+    vim.api.nvim_set_keymap("n", "<leader>ei", ":VtrOpenRunner<cr>"                     , opts)
+    vim.api.nvim_set_keymap("n", "<leader>ea", ":VtrAttachToPane<cr>"                   , opts)
+    vim.api.nvim_set_keymap("x", "<leader>ee", ":call VtrSendLinesToRunnerWrapper()<cr>", opts)
+    vim.api.nvim_set_keymap("n", "<leader>ef", ":VtrSendFile<cr>"                       , opts)
 
 end
 
