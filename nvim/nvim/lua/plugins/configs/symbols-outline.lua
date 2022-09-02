4a1ba172d0b747c4ef55bf08ae3ef252affbb794
Ryan Burda <rburda@squarespace.com>
2022-09-02 16:44:07 -0400

    VtrSendLinesToRunner psql wrapper

--- nvim/nvim/lua/plugins/configs/symbols-outline.lua
+++ nvim/nvim/lua/plugins/configs/symbols-outline.lua
@@ -11,10 +11,6 @@ function T.setup()
     vim.api.nvim_set_keymap('n', '<leader>ss', ":SymbolsOutlineOpen<cr>", opts)
     vim.api.nvim_set_keymap('n', '<leader>sx', ":SymbolsOutlineClose<cr>", opts)
 
-    -- Make the background darker.
-    -- local augroup = vim.api.nvim_create_augroup("SymbolsOutline", { clear = true })
-    -- vim.api.nvim_create_autocmd("FileType", { pattern = "Outline", command = "highlight! link NormalNC Pmenu", group = augroup })
-
 end
 
 return T
--- nvim/nvim/lua/plugins/configs/vim-tmux-runner.lua
+++ nvim/nvim/lua/plugins/configs/vim-tmux-runner.lua
@@ -1,16 +1,31 @@
 local T = {}
 
 function T.setup()
+
     vim.g.VtrStripLeadingWhitespace = 0
     vim.g.VtrClearEmptyLines = 0
     vim.g.VtrAppendNewline = 1
 
+    vim.cmd([[
+    function! VtrSendLinesToRunnerPSQL() range
+        " `q` exists the psql pager if it is up.
+        VtrSendKeysRaw q
+        " get rid of the `q` if the psql pager wasn't open.
+        VtrSendCtrlC
+        " clear the screen.
+        VtrSendCommandToRunner \! clear
+        " send the lines.
+        VtrSendLinesToRunner
+    endfunction
+    ]])
+
     local opts = { noremap=true, silent=true }
 
-    vim.api.nvim_set_keymap('n', '<leader>ei', ':VtrOpenRunner<cr>'       , opts)
-    vim.api.nvim_set_keymap('n', '<leader>ea', ':VtrAttachToPane<cr>'     , opts)
-    vim.api.nvim_set_keymap('x', '<leader>ee', ':VtrSendLinesToRunner<cr>', opts)
-    vim.api.nvim_set_keymap('n', '<leader>ef', ':VtrSendFile<cr>'         , opts)
+    vim.api.nvim_set_keymap("n", "<leader>ei", ":VtrOpenRunner<cr>"                  , opts)
+    vim.api.nvim_set_keymap("n", "<leader>ea", ":VtrAttachToPane<cr>"                , opts)
+    vim.api.nvim_set_keymap("x", "<leader>ee", ":call VtrSendLinesToRunnerPSQL()<cr>", opts)
+    vim.api.nvim_set_keymap("n", "<leader>ef", ":VtrSendFile<cr>"                    , opts)
+
 end
 
 return T
