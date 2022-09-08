aade165ab8f5ac7226eb27b58a64876378574aa2
Ryan Burda <ryanburda@gmail.com>
2022-09-08 12:05:57 -0400

    qf picker

--- nvim/nvim/lua/plugins/configs/telescope/pickers.lua
+++ nvim/nvim/lua/plugins/configs/telescope/pickers.lua
@@ -6,11 +6,24 @@ function T.quickfix_files(opts)
     local pickers = require("telescope.pickers")
     local finders = require "telescope.finders"
 
+    local getqflist_files = function()
+        local qfl = vim.fn.getqflist()
+        local files = {}
+        local n = 0
+
+        for k,v in pairs(qfl) do
+          n=n+1
+          files[n]=v.text
+        end
+
+        return files
+    end
+
     opts = opts or {}
     pickers.new(opts, {
         prompt_title = "Quickfix Files",
         finder = finders.new_table {
-            results = vim.fn.getqflist()
+            results = getqflist_files()
         },
         sorter = conf.generic_sorter(opts),
     }):find()
