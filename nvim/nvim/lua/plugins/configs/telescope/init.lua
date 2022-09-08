04145091e1f09b7d4f107095d7c8a7cec745b531
Ryan Burda <ryanburda@gmail.com>
2022-09-08 09:38:15 -0400

    telescope directory

--- nvim/nvim/lua/plugins/configs/telescope/init.lua
+++ nvim/nvim/lua/plugins/configs/telescope/init.lua
@@ -208,20 +208,4 @@ function T.setup()
 
 end
 
--- quickfix files picker
-function T.quickfix_files(opts)
-    local conf = require("telescope.config").values
-    local pickers = require("telescope.pickers")
-    local finders = require "telescope.finders"
-
-    opts = opts or {}
-    pickers.new(opts, {
-        prompt_title = "Quickfix Files",
-        finder = finders.new_table {
-            results = vim.fn.getqflist()
-        },
-        sorter = conf.generic_sorter(opts),
-    }):find()
-end
-
 return T
