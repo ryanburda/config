d95da1897ccd8668e4c7f316923aea90c65095b2
Ryan Burda <ryanburda@gmail.com>
2022-11-22 18:27:01 -0500

    cheatsh split

--- nvim/nvim/lua/plugins/configs/cheatsh.lua
+++ nvim/nvim/lua/plugins/configs/cheatsh.lua
@@ -2,6 +2,8 @@ local T = {}
 
 function T.setup()
 
+    vim.g.cheat_default_window_layout = 'split'
+
     local opts = { noremap=true, silent=false }
     vim.api.nvim_set_keymap('n', '<leader>?', ':Cheat<CR>', opts)
 
