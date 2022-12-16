71eced15c4ac01a05ca4ac713f255267794149a4
Ryan Burda <ryanburda@gmail.com>
2022-12-16 10:37:48 -0500

    scrolloff

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -6,6 +6,7 @@ vim.o.cursorline = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.scrolloff = 8
 vim.opt.cc = '120'
 vim.opt.encoding = 'UTF-8'
 vim.opt.expandtab = true
@@ -33,27 +34,28 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 local opts = { noremap=true, silent=false }
-
-vim.api.nvim_set_keymap('n', '<leader><S-Tab>', ':tabprevious<cr>'    , opts)
-vim.api.nvim_set_keymap('n', '<leader><Tab>'  , ':tabnext<cr>'        , opts)
-vim.api.nvim_set_keymap('n', '<leader>/'      , ':source $MYVIMRC<cr>', opts)
-vim.api.nvim_set_keymap('n', '<leader>J'      , ':split<cr><C-w>j'    , opts)
-vim.api.nvim_set_keymap('n', '<leader>L'      , ':vsplit<cr><C-w>l'   , opts)
-vim.api.nvim_set_keymap('n', '<leader>u'      , ':e #<cr>'            , opts)  -- open last closed buffer
-vim.api.nvim_set_keymap('n', '<leader>0'      , ':%bd|e#|bd#<cr>'     , opts)  -- close all buffers except current
-vim.api.nvim_set_keymap('n', 'L'              , 'zLgm'                , opts)  -- horizontal scroll right
-vim.api.nvim_set_keymap('n', 'H'              , 'zHgm'                , opts)  -- horizontal scroll left
-vim.api.nvim_set_keymap('n', 'n'              , 'nzz'                 , opts)  -- next occurrence of search and center
-vim.api.nvim_set_keymap('n', 'N'              , 'Nzz'                 , opts)  -- previous occurrence of search and center
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set('n', '<leader><S-Tab>', ':tabprevious<cr>'    , opts)
+vim.keymap.set('n', '<leader><Tab>'  , ':tabnext<cr>'        , opts)
+vim.keymap.set('n', '<leader>/'      , ':source $MYVIMRC<cr>', opts)
+vim.keymap.set('n', '<leader>J'      , ':split<cr><C-w>j'    , opts)
+vim.keymap.set('n', '<leader>L'      , ':vsplit<cr><C-w>l'   , opts)
+vim.keymap.set('n', '<leader>u'      , ':e #<cr>'            , opts)  -- open last closed buffer
+vim.keymap.set('n', '<leader>0'      , ':%bd|e#|bd#<cr>'     , opts)  -- close all buffers except current
+vim.keymap.set('n', 'L'              , 'zLgm'                , opts)  -- horizontal scroll right
+vim.keymap.set('n', 'H'              , 'zHgm'                , opts)  -- horizontal scroll left
+vim.keymap.set('n', 'n'              , 'nzz'                 , opts)  -- next occurrence of search and center
+vim.keymap.set('n', 'N'              , 'Nzz'                 , opts)  -- previous occurrence of search and center
 
 -- quick notes
-vim.api.nvim_set_keymap('n', '<leader>n' , ':edit ~/Documents/main.txt<cr>G$', opts)
+vim.keymap.set('n', '<leader>n' , ':edit ~/Documents/main.txt<cr>G$', opts)
 
 -- :help vim.diagnostic.*
-vim.api.nvim_set_keymap('n', '<leader>hh', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>hj', '<cmd>lua vim.diagnostic.goto_next()<CR>' , opts)
-vim.api.nvim_set_keymap('n', '<leader>hk', '<cmd>lua vim.diagnostic.goto_prev()<CR>' , opts)
-vim.api.nvim_set_keymap('n', '<leader>hl', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+vim.keymap.set('n', '<leader>hh', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+vim.keymap.set('n', '<leader>hj', '<cmd>lua vim.diagnostic.goto_next()<CR>' , opts)
+vim.keymap.set('n', '<leader>hk', '<cmd>lua vim.diagnostic.goto_prev()<CR>' , opts)
+vim.keymap.set('n', '<leader>hl', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 
 -- :help quickfix
 vim.cmd([[
@@ -72,22 +74,22 @@ function!   QuickFixOpenAll()
 endfunction
 ]])
 
-vim.api.nvim_set_keymap('n', '<leader>ko' , ':copen<CR>'                 , opts)
-vim.api.nvim_set_keymap('n', '<leader>kx' , ':cclose<CR>'                , opts)
-vim.api.nvim_set_keymap('n', '<leader>kj' , ':cnext<CR>'                 , opts)
-vim.api.nvim_set_keymap('n', '<leader>kk' , ':cprev<CR>'                 , opts)
-vim.api.nvim_set_keymap('n', '<leader>kgg', ':cfirst<CR>'                , opts)
-vim.api.nvim_set_keymap('n', '<leader>kG' , ':clast<CR>'                 , opts)
-vim.api.nvim_set_keymap('n', '<leader>ka' , ':call QuickFixOpenAll()<CR>', opts)  -- open all files in quickfix list
-vim.api.nvim_set_keymap('n', '<leader>kc' , ':cexpr []<CR>'              , opts)  -- clear quickfix list
+vim.keymap.set('n', '<leader>ko' , ':copen<CR>'                 , opts)
+vim.keymap.set('n', '<leader>kx' , ':cclose<CR>'                , opts)
+vim.keymap.set('n', '<leader>kj' , ':cnext<CR>'                 , opts)
+vim.keymap.set('n', '<leader>kk' , ':cprev<CR>'                 , opts)
+vim.keymap.set('n', '<leader>kgg', ':cfirst<CR>'                , opts)
+vim.keymap.set('n', '<leader>kG' , ':clast<CR>'                 , opts)
+vim.keymap.set('n', '<leader>ka' , ':call QuickFixOpenAll()<CR>', opts)  -- open all files in quickfix list
+vim.keymap.set('n', '<leader>kc' , ':cexpr []<CR>'              , opts)  -- clear quickfix list
 
 -- :help location-list
-vim.api.nvim_set_keymap('n', '<leader>lo' , ':lopen<CR>'   , opts)
-vim.api.nvim_set_keymap('n', '<leader>lx' , ':lclose<CR>'  , opts)
-vim.api.nvim_set_keymap('n', '<leader>lj' , ':lnext<CR>'   , opts)
-vim.api.nvim_set_keymap('n', '<leader>lk' , ':lprev<CR>'   , opts)
-vim.api.nvim_set_keymap('n', '<leader>lgg', ':lfirst<CR>'  , opts)
-vim.api.nvim_set_keymap('n', '<leader>lG' , ':llast<CR>'   , opts)
-vim.api.nvim_set_keymap('n', '<leader>lc' , ':lexpr []<CR>', opts)  -- clear location-list
+vim.keymap.set('n', '<leader>lo' , ':lopen<CR>'   , opts)
+vim.keymap.set('n', '<leader>lx' , ':lclose<CR>'  , opts)
+vim.keymap.set('n', '<leader>lj' , ':lnext<CR>'   , opts)
+vim.keymap.set('n', '<leader>lk' , ':lprev<CR>'   , opts)
+vim.keymap.set('n', '<leader>lgg', ':lfirst<CR>'  , opts)
+vim.keymap.set('n', '<leader>lG' , ':llast<CR>'   , opts)
+vim.keymap.set('n', '<leader>lc' , ':lexpr []<CR>', opts)  -- clear location-list
 
 require('plugins')
