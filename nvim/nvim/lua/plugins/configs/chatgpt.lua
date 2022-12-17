bea93a941ed02bdba468e63df83e370ff4882908
Ryan Burda <ryanburda@gmail.com>
2022-12-17 10:55:54 -0500

    chatgpt env variable

--- nvim/nvim/lua/plugins/configs/chatgpt.lua
+++ nvim/nvim/lua/plugins/configs/chatgpt.lua
@@ -2,7 +2,9 @@ local T = {}
 
 function T.setup()
 
-    require("chatgpt").setup()
+    require("chatgpt").setup({
+        welcome_message = ""
+    })
 
     local opts = { noremap=true, silent=false }
     vim.keymap.set("n", "<leader>cb", ':ChatGPT<CR>', opts)
