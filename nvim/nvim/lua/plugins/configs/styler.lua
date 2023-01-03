317d06d30f72d043e5c1d71d140abc1b54d07adf
Ryan Burda <ryanburda@gmail.com>
2023-01-03 07:03:46 -0500

    vim pager, man page colorscheme, symbols outline ts colors

--- nvim/nvim/lua/plugins/configs/styler.lua
+++ nvim/nvim/lua/plugins/configs/styler.lua
@@ -4,6 +4,7 @@ function T.setup()
 
     require("styler").setup({
         themes = {
+            man = { colorscheme = "carbonfox" },
             help = { colorscheme = "carbonfox" },
             markdown = { colorscheme = "nordfox" },
         },
--- nvim/nvim/lua/plugins/init.lua
+++ nvim/nvim/lua/plugins/init.lua
@@ -153,7 +153,38 @@ return {
         config = function ()
             require("symbols-outline").setup({
                 width = 20,
-            }) end,
+                autofold_depth = 1,
+                -- TODO: remove once https://github.com/simrat39/symbols-outline.nvim/pull/191/files is merged.
+                symbols = {
+                    File = {hl = "@text.uri" },
+                    Module = {hl = "@namespace" },
+                    Namespace = {hl = "@namespace" },
+                    Package = {hl = "@namespace" },
+                    Class = {hl = "@type" },
+                    Method = {hl = "@method" },
+                    Property = {hl = "@method" },
+                    Field = {hl = "@field" },
+                    Constructor = {hl = "@constructor" },
+                    Enum = {hl = "@type" },
+                    Interface = {hl = "@type" },
+                    Function = {hl = "@function" },
+                    Variable = {hl = "@constant" },
+                    Constant = {hl = "@constant" },
+                    String = {hl = "@string" },
+                    Number = {hl = "@number" },
+                    Boolean = {hl = "@boolean" },
+                    Array = {hl = "@constant" },
+                    Object = {hl = "@type" },
+                    Key = {hl = "@type" },
+                    Null = {hl = "@type" },
+                    EnumMember = {hl = "@field" },
+                    Struct = {hl = "@type" },
+                    Event = {hl = "@type" },
+                    Operator = {hl = "@operator" },
+                    TypeParameter = {hl = "@parameter" },
+                },
+            })
+        end,
     },
 
     -- Tmux
