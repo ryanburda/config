28aca9cdebf3cf0b84fbd45ecaa2b4d835465a20
Ryan Burda <ryanburda@gmail.com>
2023-01-04 11:07:46 -0500

    decreased number of zsh completion lines

--- nvim/nvim/lua/plugins/init.lua
+++ nvim/nvim/lua/plugins/init.lua
@@ -224,7 +224,7 @@ return {
         lazy = true,
         keys = {
             { "<leader>ji", ":BrowserSearch<cr>", desc = "Search for word under cursor in browser" },
-            { "<leader>ji", ":'<,'>BrowserSearch<cr>", desc = "Search for visual selection in brower" },
+            { "<leader>ji", ":'<,'>BrowserSearch<cr>", desc = "Search for visual selection in brower", mode = {"v"} },
         },
     },
 
@@ -315,7 +315,7 @@ return {
                 sources = {
                     null_ls.builtins.formatting.stylua,
                     null_ls.builtins.completion.spell,
-                    null_ls.builtins.diagnostics.flake8,
+                    --null_ls.builtins.diagnostics.flake8,
                 },
             })
         end,
