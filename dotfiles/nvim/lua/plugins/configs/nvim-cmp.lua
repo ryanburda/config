5c3737346b20cbc8d12d225c62b25fc0730c8512
Ryan Burda <ryanburda@gmail.com>
2023-02-08 07:12:59 -0500

    cmp no longer shows up on new lines

--- dotfiles/nvim/lua/plugins/configs/nvim-cmp.lua
+++ dotfiles/nvim/lua/plugins/configs/nvim-cmp.lua
@@ -5,10 +5,28 @@ function T.setup()
     local cmp = require('cmp')
     local lspkind = require('lspkind')
 
+    local is_whitespace = function()
+        -- returns true if the character under the cursor is whitespace.
+        local col = vim.fn.col('.') - 1
+        local line = vim.fn.getline('.')
+        local char_under_cursor = string.sub(line, col, col)
+
+        if col == 0 or string.match(char_under_cursor, '%s') then
+            return true
+        else
+            return false
+        end
+    end
+
+    local is_comment = function()
+        -- uses treesitter to determine if cursor is currently in a comment.
+        local context = require("cmp.config.context")
+        return context.in_treesitter_capture("comment") == true or context.in_syntax_group("Comment")
+    end
+
     cmp.setup({
         enabled = function()
-            -- Don't show completion in comments
-            if require"cmp.config.context".in_treesitter_capture("comment")==true or require"cmp.config.context".in_syntax_group("Comment") then
+            if is_comment() or is_whitespace() then
                 return false
             else
                 return true
