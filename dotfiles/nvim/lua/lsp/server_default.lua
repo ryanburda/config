dfdde6030452da6a2ff5e136ffe204adc537d618
Ryan Burda <ryanburda@gmail.com>
2023-03-06 04:16:17 -0500

    lsp keymaps

--- dotfiles/nvim/lua/lsp/server_default.lua
+++ dotfiles/nvim/lua/lsp/server_default.lua
@@ -4,13 +4,12 @@ T.capabilities = vim.lsp.protocol.make_client_capabilities()
 T.capabilities = require('cmp_nvim_lsp').default_capabilities(T.capabilities)
 
 function T.on_attach(_, bufnr)
-    local opts = { noremap=true, silent=false}
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'H', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>jj', '<cmd>lua vim.lsp.buf.definition()<CR>'     , opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>jt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>jk', '<cmd>lua vim.lsp.buf.references()<CR>'     , opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ja', '<cmd>lua vim.lsp.buf.code_action()<CR>'    , opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>jr', '<cmd>lua vim.lsp.buf.rename()<CR>'         , opts)
+    vim.keymap.set('n', '<leader>jh', '<cmd>lua vim.lsp.buf.hover()<CR>', {desc = 'LSP Hover', buffer = bufnr})
+    vim.keymap.set('n', '<leader>jd', '<cmd>lua vim.lsp.buf.definition()<CR>', {desc = 'LSP jump to definition', buffer = bufnr})
+    vim.keymap.set('n', '<leader>jt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', {desc = 'LSP jump to type', buffer = bufnr})
+    vim.keymap.set('n', '<leader>jk', '<cmd>lua vim.lsp.buf.references()<CR>', {desc = 'LSP references kwickfix', buffer = bufnr})
+    vim.keymap.set('n', '<leader>ja', '<cmd>lua vim.lsp.buf.code_action()<CR>', {desc = 'LSP code action', buffer = bufnr})
+    vim.keymap.set('n', '<leader>jr', '<cmd>lua vim.lsp.buf.rename()<CR>', {desc = 'LSP rename', buffer = bufnr})
 end
 
 -- Setup the config
