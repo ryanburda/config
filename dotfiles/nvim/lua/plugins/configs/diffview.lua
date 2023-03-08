7a3b1c90fd3d5dedfa623328beaf23ba2777bfa8
Ryan Burda <ryanburda@gmail.com>
2023-03-08 08:41:16 -0500

    default diffview file_panel

--- dotfiles/nvim/lua/plugins/configs/diffview.lua
+++ dotfiles/nvim/lua/plugins/configs/diffview.lua
@@ -6,17 +6,6 @@ function T.setup()
 
     require("diffview").setup({
         enhanced_diff_hl = true,
-        file_panel = {
-            listing_style = "tree",
-            tree_options = {
-                flatten_dirs = true,
-                folder_statuses = "only_folded",
-            },
-            win_config = {
-                position = "top",
-                height = 15
-            },
-        },
         keymaps = {
             view = {
                 ["<leader>dh"] = actions.conflict_choose("ours"),
