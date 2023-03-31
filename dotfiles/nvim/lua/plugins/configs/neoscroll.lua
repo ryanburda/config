f096c7c6c41a71683f41086bd35b381057e7f505
Ryan Burda <ryanburda@gmail.com>
2023-03-31 05:34:42 -0400

    neoscroll speed

--- dotfiles/nvim/lua/plugins/configs/neoscroll.lua
+++ dotfiles/nvim/lua/plugins/configs/neoscroll.lua
@@ -9,13 +9,13 @@ function T.setup()
 
     local t = {}
     -- Syntax: t[keys] = {function, {function arguments}}
-    t['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '90'}}
-    t['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '90'}}
-    t['<C-y>'] = {'scroll', {'-0.10', 'false', '90'}}
-    t['<C-e>'] = {'scroll', { '0.10', 'false', '90'}}
-    t['zt']    = {'zt', {'90'}}
-    t['zz']    = {'zz', {'90'}}
-    t['zb']    = {'zb', {'90'}}
+    t['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '95'}}
+    t['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '95'}}
+    t['<C-y>'] = {'scroll', {'-0.10', 'false', '95'}}
+    t['<C-e>'] = {'scroll', { '0.10', 'false', '95'}}
+    t['zt']    = {'zt', {'95'}}
+    t['zz']    = {'zz', {'95'}}
+    t['zb']    = {'zb', {'95'}}
 
     require('neoscroll.config').set_mappings(t)
 
