e5a6909712d558cf93ad0512288d5babb1c8f7cf
Ryan Burda <ryanburda@gmail.com>
2023-05-24 15:50:39 -0400

    neotree symbols working

--- dotfiles/nvim/lua/plugins/init.lua
+++ dotfiles/nvim/lua/plugins/init.lua
@@ -70,39 +70,18 @@ return {
         },
         config = function()
             require("neo-tree").setup({
+                sources = { "filesystem", "document_symbols", },
                 filesystem = {
                     follow_current_file = true,
-                },
-                document_symbols = {
-                    kinds = {
-                        File = { icon = "󰈙", hl = "Tag" },
-                        Namespace = { icon = "󰌗", hl = "Include" },
-                        Package = { icon = "󰏖", hl = "Label" },
-                        Class = { icon = "󰌗", hl = "Include" },
-                        Property = { icon = "󰆧", hl = "@property" },
-                        Enum = { icon = "󰒻", hl = "@number" },
-                        Function = { icon = "󰊕", hl = "Function" },
-                        String = { icon = "󰀬", hl = "String" },
-                        Number = { icon = "󰎠", hl = "Number" },
-                        Array = { icon = "󰅪", hl = "Type" },
-                        Object = { icon = "󰅩", hl = "Type" },
-                        Key = { icon = "󰌋", hl = "" },
-                        Struct = { icon = "󰌗", hl = "Type" },
-                        Operator = { icon = "󰆕", hl = "Operator" },
-                        TypeParameter = { icon = "󰊄", hl = "Type" },
-                        StaticMethod = { icon = '󰠄 ', hl = 'Function' },
-                    }
                 },
                 source_selector = {
                     winbar = true,
                     sources = {
                         { source = "filesystem", display_name = " 󰉓  Files " },
-                        { source = "buffers", display_name = "   Buffers" },
                         { source = "document_symbols", display_name = "   Symbols " },
                     },
                 },
             })
-
 
             vim.keymap.set('n', "<leader>aa", ":Neotree toggle<CR>")
         end
@@ -111,7 +90,13 @@ return {
     -- Scrolling
     {  -- Scrollbar
         'petertriho/nvim-scrollbar',
-        config = function() require("scrollbar").setup() end
+        config = function() require("scrollbar").setup({
+            marks = {
+                Cursor = {
+                    text = "",
+                },
+            },
+        }) end
     },
     {  -- Smooth scrolling
         'karb94/neoscroll.nvim',
