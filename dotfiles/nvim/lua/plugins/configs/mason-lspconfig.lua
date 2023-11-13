752559ba14f80936d29d0fecbb8d32cec612a915
Ryan Burda <ryanburda@gmail.com>
2023-11-13 05:47:00 -0500

    indent blank lines v3

--- dotfiles/nvim/lua/plugins/configs/mason-lspconfig.lua
+++ dotfiles/nvim/lua/plugins/configs/mason-lspconfig.lua
@@ -149,6 +149,7 @@ function T.setup()
                     on_attach = function(_, bufnr)
                         on_attach(_, bufnr)
                         vim.keymap.set("n", "<M-m>", rt.hover_actions.hover_actions, { buffer = bufnr })
+                        vim.keymap.set("n", "<M-Space>", rt.hover_actions.hover_actions, { buffer = bufnr })
                         vim.keymap.set("n", "<M-,>", rt.code_action_group.code_action_group, { buffer = bufnr })
                     end,
                 },
--- dotfiles/nvim/lua/plugins/init.lua
+++ dotfiles/nvim/lua/plugins/init.lua
@@ -248,10 +248,7 @@ return {
     {
         'lukas-reineke/indent-blankline.nvim',
         config = function ()
-            require("indent_blankline").setup {
-                space_char_blankline = " ",
-                show_current_context = true,
-            }
+            require("ibl").setup {}
         end
     },
 
