3e3b460e96fe01b32211b1fea360387b25897f6c
Ryan Burda <ryanburda@gmail.com>
2023-12-26 08:54:01 -0500

    neovim install from brew
    s

--- setup.sh
+++ setup.sh
@@ -27,6 +27,7 @@ brew install --cask shifty
 brew install coreutils
 brew install tmux
 brew install tmate
+brew install neovim
 brew install tmuxinator
 brew install ninja
 brew install libtool
@@ -104,37 +105,39 @@ SCRIPT_DIR=${0:a:h}
 
 mkdir -p ~/.config/alacritty
 
-ln -svfF "${SCRIPT_DIR}/dotfiles/zshrc" "${HOME}/.zshrc"
-mkdir -p "${HOME}/.zsh/funcs"
-ln -svfF "${SCRIPT_DIR}/dotfiles/funcs/find_file" "${HOME}/.zsh/funcs/find_file"
-ln -svfF "${SCRIPT_DIR}/dotfiles/funcs/set_font" "${HOME}/.zsh/funcs/set_font"
-ln -svfF "${SCRIPT_DIR}/dotfiles/funcs/font_picker" "${HOME}/.zsh/funcs/font_picker"
-ln -svfF "${SCRIPT_DIR}/dotfiles/funcs/nvim_open" "${HOME}/.zsh/funcs/nvim_open"
-ln -svfF "${SCRIPT_DIR}/dotfiles/funcs/psqlp" "${HOME}/.zsh/funcs/psqlp"
+ln -svfF "${SCRIPT_DIR}/dotfiles/zshrc"                "${HOME}/.zshrc"
+ln -svfF "${SCRIPT_DIR}/dotfiles/gitconfig"            "${HOME}/.gitconfig"
+ln -svfF "${SCRIPT_DIR}/dotfiles/psqlrc"               "${HOME}/.psqlrc"
+ln -svfF "${SCRIPT_DIR}/dotfiles/pspgconf"             "${HOME}/.pspgconf"
+ln -svfF "${SCRIPT_DIR}/dotfiles/octaverc"             "${HOME}/.octaverc"
+mkdir -p                                               "${HOME}/.zsh/funcs"
+ln -svfF "${SCRIPT_DIR}/dotfiles/funcs/find_file"      "${HOME}/.zsh/funcs/find_file"
+ln -svfF "${SCRIPT_DIR}/dotfiles/funcs/set_font"       "${HOME}/.zsh/funcs/set_font"
+ln -svfF "${SCRIPT_DIR}/dotfiles/funcs/font_picker"    "${HOME}/.zsh/funcs/font_picker"
+ln -svfF "${SCRIPT_DIR}/dotfiles/funcs/nvim_open"      "${HOME}/.zsh/funcs/nvim_open"
+ln -svfF "${SCRIPT_DIR}/dotfiles/funcs/psqlp"          "${HOME}/.zsh/funcs/psqlp"
 ln -svfF "${SCRIPT_DIR}/dotfiles/funcs/select_session" "${HOME}/.zsh/funcs/select_session"
-ln -svfF "${SCRIPT_DIR}/dotfiles/funcs/theme_picker" "${HOME}/.zsh/funcs/theme_picker"
-ln -svfF "${SCRIPT_DIR}/dotfiles/funcs/tse" "${HOME}/.zsh/funcs/tse"
+ln -svfF "${SCRIPT_DIR}/dotfiles/funcs/theme_picker"   "${HOME}/.zsh/funcs/theme_picker"
+ln -svfF "${SCRIPT_DIR}/dotfiles/funcs/tse"            "${HOME}/.zsh/funcs/tse"
 
-mkdir -p "${XDG_CONFIG_HOME}/lf"
+ln -svfF "${SCRIPT_DIR}/dotfiles/alacritty.yml.template"                       "${XDG_CONFIG_HOME}/alacritty/alacritty.yml.template"
+ln -svfF "${SCRIPT_DIR}/dotfiles/karabiner.json"                               "${XDG_CONFIG_HOME}/karabiner/karabiner.json"
+ln -svfF "${SCRIPT_DIR}/dotfiles/lazygit.yml"                                  "${XDG_CONFIG_HOME}/lazygit/config.yml"
+mkdir -p                                                                       "${XDG_CONFIG_HOME}/lf"
 curl https://raw.githubusercontent.com/gokcehan/lf/master/etc/icons.example -o "${XDG_CONFIG_HOME}/lf/icons"
-ln -svfF "${SCRIPT_DIR}/dotfiles/lfrc" "${XDG_CONFIG_HOME}/lf/lfrc"
-
-mkdir -p "${XDG_CONFIG_HOME}/tmux"
-ln -svfF "${SCRIPT_DIR}/dotfiles/tmux.conf" "${XDG_CONFIG_HOME}/tmux/tmux.conf"
-ln -svfF "${SCRIPT_DIR}/dotfiles/alacritty.yml.template" "${XDG_CONFIG_HOME}/alacritty/alacritty.yml.template"
-ln -svfF "${SCRIPT_DIR}/dotfiles/karabiner.json" "${XDG_CONFIG_HOME}/karabiner/karabiner.json"
-ln -svfF "${SCRIPT_DIR}/dotfiles/lazygit.yml" "${XDG_CONFIG_HOME}/lazygit/config.yml"
-
-ln -svfF "${SCRIPT_DIR}/dotfiles/gitconfig" "${HOME}/.gitconfig"
-ln -svfF "${SCRIPT_DIR}/dotfiles/psqlrc" "${HOME}/.psqlrc"
-ln -svfF "${SCRIPT_DIR}/dotfiles/pspgconf" "${HOME}/.pspgconf"
-ln -svfF "${SCRIPT_DIR}/dotfiles/octaverc" "${HOME}/.octaverc"
-ln -svfF "${SCRIPT_DIR}/dotfiles/nord-status-content.conf" "${HOME}/.config/tmux/plugins/tmux/src/nord-status-content.conf"
-
-source-file "${HOME}/.config/tmux/plugins/tmux/src/nord-status-content.conf"
+ln -svfF "${SCRIPT_DIR}/dotfiles/lfrc"                                         "${XDG_CONFIG_HOME}/lf/lfrc"
+mkdir -p                                                                       "${XDG_CONFIG_HOME}/nvim"
+ln -svfF "${SCRIPT_DIR}/dotfiles/nvim"                                         "${XDG_CONFIG_HOME}/nvim"
+mkdir -p                                                                       "${XDG_CONFIG_HOME}/tmux"
+ln -svfF "${SCRIPT_DIR}/dotfiles/tmux.conf"                                    "${XDG_CONFIG_HOME}/tmux/tmux.conf"
+ln -svfF "${SCRIPT_DIR}/dotfiles/nord-status-content.conf"                     "${XDG_CONFIG_HOME}/tmux/plugins/tmux/src/nord-status-content.conf"
+source-file                                                                    "${XDG_CONFIG_HOME}/tmux/plugins/tmux/src/nord-status-content.conf"
 
 # Obsidian
 ln -svfF "${SCRIPT_DIR}/dotfiles/obsidian.vimrc" "${HOME}/Documents/notes/notes/.obsidian.vimrc"
+
+# Let nvim set itself up now that the config has been linked
+nvim --headless "+Lazy! sync" +qa
 
 # Tmux Plugin Manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -144,10 +147,8 @@ source ${HOME}/.zshrc
 
 # cloud-sql-proxy
 brew install --cask google-cloud-sdk
-
 CPU=$(sysctl -n machdep.cpu.brand_string)
 CLOUD_SQL_PROXY_PATH=$HOME/.local/bin/cloud_sql_proxy 
-
 if [[ $CPU =~ ^Apple ]]; then
     curl -o $CLOUD_SQL_PROXY_PATH https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.arm64
     chmod +x $CLOUD_SQL_PROXY_PATH
@@ -158,39 +159,6 @@ else
     echo "WARNING: could not determine which cloud-sql-proxy version to install."
 fi
 
-# alacritty color schemes
-git clone https://github.com/eendroroy/alacritty-theme.git ~/.alacritty-colorscheme
-
 # Show task switcher on all monitors.
 defaults write com.apple.Dock appswitcher-all-displays -bool true
 killall Dock
-
-################
-# Setup Neovim #
-################
-NVIM_REPO_PATH="$HOME/.nvim/neovim"
-NVIM_PLUGINS_PATH="$HOME/.nvim/plugins"  # empty directory where plugins can be installed and tested locally.
-NVIM_INSTALL_DIR_PATH="$HOME/.local/bin/neovim"
-
-mkdir -p $NVIM_PLUGINS_PATH
-mkdir -p $NVIM_INSTALL_DIR_PATH
-
-# Build Prerequisites
-brew install --quiet ninja libtool automake cmake pkg-config gettext curl
-
-# Clone or pull the neovim repo
-git clone git@github.com:neovim/neovim.git $NVIM_REPO_PATH 2> /dev/null || git -C $NVIM_REPO_PATH pull
-
-# Remove any files that neovim may have created previously
-sudo rm -rf "$HOME/.local/share/nvim"
-
-# Install neovim
-cd $NVIM_REPO_PATH && make CMAKE_BUILD_TYPE=RelWithDebInfo
-sudo make distclean
-sudo make install
-
-# link nvim config
-ln -svfF $SCRIPT_DIR/dotfiles/nvim $HOME/.config/nvim
-
-# Let nvim set itself up now that the config has been linked
-nvim --headless "+Lazy! sync" +qa
