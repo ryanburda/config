86979d5758b2a1e9854239ccab5fce0620dd7244
Ryan Burda <ryanburda@gmail.com>
2024-01-06 20:05:27 -0500

    ordered brew installs

--- setup.sh
+++ setup.sh
@@ -16,72 +16,72 @@ else
     brew update
 fi
 
-# Applications
-brew install --cask alacritty
-brew install --cask docker
-brew install --cask karabiner-elements
-brew install --cask alfred
-brew install --cask rectangle
-brew install --cask shifty
-
-brew install coreutils
-brew install tmux
-brew install tmate
-brew install neovim
-brew install tmuxinator
-brew install ninja
-brew install libtool
+brew install aichat
 brew install automake
-brew install cmake
-brew install pkg-config
-brew install gettext
-brew install curl
-brew install lsd
-brew install bottom
 brew install bat
-brew install fzf
-brew install sk
-brew install fzy
-brew install wget
-brew install go
-brew install php
+brew install bottom
+brew install cmake
 brew install composer
-brew install lazygit
-brew install lazydocker
-brew install k9s
+brew install coreutils
+brew install curl
+brew install deno
+brew install diff-so-fancy
 brew install fd
+brew install fzf
+brew install fzy
+brew install gettext
+brew install go
 brew install htop
-brew install node
-brew install lua
-brew install ripgrep
 brew install jq
-brew install ttyrec
-brew install rlwrap
-brew install lf
+brew install k9s
 brew install kubectl
 brew install kubectx
-brew install pspg
-brew install diff-so-fancy
-brew install deno
-brew install pyenv
-brew install pyenv-virtualenv
+brew install lazydocker
+brew install lazygit
+brew install lf
 brew install libpq
 brew link --force libpq
+brew install libtool
+brew install lsd
+brew install lua
+brew install neovim
+brew install ninja
+brew install node
 brew install octave
-brew install aichat
-brew install zsh-vi-mode
-brew install zsh-syntax-highlighting
+brew install php
+brew install pkg-config
+brew install pspg
+brew install pyenv
+brew install pyenv-virtualenv
+brew install ripgrep
+brew install rlwrap
+brew install sk
+brew install tmate
+brew install tmux
+brew install tmuxinator
+brew install ttyrec
+brew install wget
 brew install zsh-autosuggestions
+brew install zsh-syntax-highlighting
+brew install zsh-vi-mode
 
 # Fonts
 brew tap homebrew/cask-fonts
-brew install --cask font-sauce-code-pro-nerd-font
-brew install --cask font-hack-nerd-font
 brew install --cask font-fira-code-nerd-font
-brew install --cask font-jetbrains-mono-nerd-font
 brew install --cask font-gohufont-nerd-font
+brew install --cask font-hack-nerd-font
+brew install --cask font-jetbrains-mono-nerd-font
+brew install --cask font-sauce-code-pro-nerd-font
 brew tap shaunsingh/SFMono-Nerd-Font-Ligaturized
 brew install --cask font-sf-mono-nerd-font-ligaturized
+
+# Applications
+brew install --cask alacritty
+brew install --cask alfred
+brew install --cask docker
+brew install --cask karabiner-elements
+brew install --cask rectangle
+brew install --cask shifty
 
 # rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
