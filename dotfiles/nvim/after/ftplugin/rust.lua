e2dd68abfcfff581af66a6e7d914a274286689dc
Ryan Burda <ryanburda@gmail.com>
2024-01-29 09:09:29 -0500

    rust debuggables

---
+++ dotfiles/nvim/after/ftplugin/rust.lua
@@ -0,0 +1,13 @@
+local bufnr = vim.api.nvim_get_current_buf()
+
+-- mrcjkb/rustaceanvim automatically detects codelldb installations.
+-- In this case codelldb should be installed from Mason.
+-- See: https://github.com/mrcjkb/rustaceanvim?tab=readme-ov-file#using-codelldb-for-debugging
+vim.keymap.set(
+    "n",
+    "<M-d>",
+    function()
+        vim.cmd.RustLsp('debuggables')
+    end,
+    { buffer = bufnr }
+)
