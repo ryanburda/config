f1354928106931e05e1f7bf5d885e580c249d4dd
Ryan Burda <ryanburda@gmail.com>
2024-01-30 05:28:32 -0500

    rust-analyzer settings

--- dotfiles/nvim/after/ftplugin/rust.lua
+++ dotfiles/nvim/after/ftplugin/rust.lua
@@ -1,5 +1,25 @@
 local bufnr = vim.api.nvim_get_current_buf()
 
+vim.g.rustaceanvim = {
+    server = {
+        settings = {
+            -- rust-analyzer language server configuration
+            ['rust-analyzer'] = {
+                cargo = { allFeatures = true },
+                checkOnSave = true,
+                check = {
+                    enable = true,
+                    command = 'clippy',
+                    features = 'all',
+                },
+                procMacro = {
+                    enable = true,
+                },
+            },
+        },
+    },
+}
+
 -- mrcjkb/rustaceanvim automatically detects codelldb installations.
 -- In this case codelldb should be installed from Mason.
 -- See: https://github.com/mrcjkb/rustaceanvim?tab=readme-ov-file#using-codelldb-for-debugging
