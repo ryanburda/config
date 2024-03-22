010f24dcc51a32273a9efd6394cc30007d5a4090
Ryan Burda <ryanburda@gmail.com>
2024-03-22 10:35:25 -0400

    cloud-sql-proxy

--- dotfiles/scripts/install_cloud_sql_proxy.sh
+++ dotfiles/scripts/install_cloud_sql_proxy.sh
@@ -2,13 +2,11 @@
 brew install --cask google-cloud-sdk
 
 CPU=$(sysctl -n machdep.cpu.brand_string)
-CLOUD_SQL_PROXY_PATH=usr/local/bin/cloud_sql_proxy
+CLOUD_SQL_PROXY_PATH=/usr/local/bin
 if [[ $CPU =~ ^Apple ]]; then
-    curl -o $CLOUD_SQL_PROXY_PATH https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.arm64
-    chmod +x $CLOUD_SQL_PROXY_PATH
+    (cd $CLOUD_SQL_PROXY_PATH && curl -o cloud-sql-proxy https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v2.10.1/cloud-sql-proxy.darwin.arm64 && chmod +x cloud-sql-proxy)
 elif [[ $CPU =~ ^Intel ]]; then
-    curl -o $CLOUD_SQL_PROXY_PATH https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.amd64
-    chmod +x $CLOUD_SQL_PROXY_PATH
+    (cd $CLOUD_SQL_PROXY_PATH && curl -o cloud-sql-proxy https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v2.10.1/cloud-sql-proxy.darwin.amd64 && chmod +x cloud-sql-proxy)
 else
     echo "WARNING: could not determine which cloud-sql-proxy version to install."
 fi
