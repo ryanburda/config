959bc317440885dbb46fcebd6050e533b52b7f51
Ryan Burda <ryanburda@gmail.com>
2024-03-27 23:28:09 -0400

    remove default server config

--- dotfiles/nvim/lua/plugins/configs/mason-lspconfig.lua
+++ dotfiles/nvim/lua/plugins/configs/mason-lspconfig.lua
@@ -23,11 +23,11 @@ function T.setup()
         -- The first entry (without a key) will be the default handler
         -- and will be called for each installed server that doesn't have
         -- a dedicated handler.
-        function (server_name) -- default handler (optional)
-            require("lspconfig")[server_name].setup({
-                capabilities = capabilities
-            })
-        end,
+        -- function (server_name) -- default handler (optional)
+        --     require("lspconfig")[server_name].setup({
+        --         capabilities = capabilities
+        --     })
+        -- end,
         -- Next, you can provide a dedicated handler for specific servers.
         ["lua_ls"] = function ()
             require("neodev").setup({
