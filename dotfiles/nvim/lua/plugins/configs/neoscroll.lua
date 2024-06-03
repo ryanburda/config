2fba8d75b2bfce05921b3e41adcd601b9c3934a3
Ryan Burda <ryanburda@gmail.com>
2024-06-02 23:07:09 -0400

    neoscroll gg

--- dotfiles/nvim/lua/plugins/configs/neoscroll.lua
+++ dotfiles/nvim/lua/plugins/configs/neoscroll.lua
@@ -10,11 +10,6 @@ function T.setup()
       ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 10 }) end;
       ["<C-y>"] = function() neoscroll.scroll(-0.1, { move_cursor=false; duration = 100 }) end;
       ["<C-e>"] = function() neoscroll.scroll(0.1, { move_cursor=false; duration = 100 }) end;
-      ["zt"]    = function() neoscroll.zt({ half_screen_duration = 10, half_win_duration = 10 }) end;
-      ["zz"]    = function() neoscroll.zz({ half_screen_duration = 10, half_win_duration = 10}) end;
-      ["zb"]    = function() neoscroll.zb({ half_screen_duration = 10, half_win_duration = 10 }) end;
-      ["G"]     = function() neoscroll.G({ half_screen_duration = 10, half_win_duration = 10 }) end;
-      ["gg"]    = function() neoscroll.gg({ half_screen_duration = 10, half_win_duration = 10 }) end;
     }
     local modes = { 'n', 'v', 'x' }
     for key, func in pairs(keymap) do
