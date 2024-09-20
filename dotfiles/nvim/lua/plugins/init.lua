6df5c42baea63e56ad80d548157777117a305811
Ryan Burda <ryanburda@gmail.com>
2024-09-20 07:55:02 -0400

    nvim-dbee

--- dotfiles/nvim/lua/plugins/init.lua
+++ dotfiles/nvim/lua/plugins/init.lua
@@ -363,6 +363,22 @@ return {
     "tpope/vim-dadbod",
     "kristijanhusak/vim-dadbod-completion",
     "kristijanhusak/vim-dadbod-ui",
+    {
+        "kndndrj/nvim-dbee",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+        },
+        build = function()
+            -- Install tries to automatically detect the install method.
+            -- if it fails, try calling it with one of these parameters:
+            --    "curl", "wget", "bitsadmin", "go"
+            require("dbee").install()
+        end,
+        config = function()
+            require("dbee").setup(--[[optional config]])
+            vim.keymap.set('n', '<M-d>', "<cmd>lua require('dbee').toggle()<cr>", {desc = "Toggle nvim-dbee"})
+        end,
+    },
 
     -- Github Copilot
     {
