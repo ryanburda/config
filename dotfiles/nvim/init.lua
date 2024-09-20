40d8fa330ea69b48fc842665777261fd15783b45
Ryan Burda <ryanburda@gmail.com>
2024-09-20 18:20:04 -0400

    highlight yank text

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -50,6 +50,17 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
     end
 })
 
+-- Highlight when yanking text.
+-- Try it with `yap` in normal mode
+-- See: `:help.vim.highlihgt.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking text',
+    group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
 -- keymap
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
