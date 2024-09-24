961354c054aeac8bbeac85eac29caee6cdc7f35e
Ryan Burda <ryanburda@gmail.com>
2024-09-24 10:23:27 -0400

    removed nvim-dbee, sql completion working

--- dotfiles/nvim/lua/plugins/init.lua
+++ dotfiles/nvim/lua/plugins/init.lua
@@ -392,39 +392,6 @@ return {
 
     -- sql
     "nanotee/sqls.nvim",
-    {
-        "kndndrj/nvim-dbee",
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-        },
-        build = function()
-            -- Install tries to automatically detect the install method.
-            -- if it fails, try calling it with one of these parameters:
-            --    "curl", "wget", "bitsadmin", "go"
-            require("dbee").install()
-        end,
-        config = function()
-            require("dbee").setup(--[[optional config]])
-        end,
-    },
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            {
-                "MattiasMTS/cmp-dbee",
-                dependencies = {
-                    {"kndndrj/nvim-dbee"}
-                },
-                ft = "sql", -- optional but good to have
-                opts = {}, -- needed
-            },
-        },
-        opts = {
-            sources = {
-                { "cmp-dbee" },
-            },
-        },
-    },
 
     -- Github Copilot
     {
