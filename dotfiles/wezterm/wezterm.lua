f846223e048da4916c7655dc3fca5d50979399bd
Ryan Burda <ryanburda@gmail.com>
2024-09-27 13:52:02 -0400

    font_picker working in wezterm

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -8,12 +8,27 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
+local function get_var_from_file(file_path)
+    local file, err = io.open(file_path, "r")
+    if not file then
+        print("Could not open file: " .. err)
+        return
+    end
+
+    local content = file:read("*a")
+    -- Remove trailing new lines
+    content = content:gsub("%s*$", "")
+
+    file:close()
+    return content
+end
+
 -- colors
 config.color_scheme = "nord"
 
 -- font
-config.font = wezterm.font("JetBrains Mono NL")
-config.font_size = 14
+config.font = wezterm.font(get_var_from_file("/Users/ryanburda/.config/alacritty/.font"))
+config.font_size = tonumber(get_var_from_file("/Users/ryanburda/.config/alacritty/.font_size"))
 
 config.window_decorations = "RESIZE"
 
@@ -22,6 +37,7 @@ config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = false
 config.use_fancy_tab_bar = false
 config.tab_and_split_indices_are_zero_based = true
+config.window_close_confirmation = 'NeverPrompt'
 
 -- and finally, return the configuration to wezterm
 return config
