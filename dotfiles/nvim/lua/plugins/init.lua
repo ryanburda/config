338763434c3effb4403cb03e202cb5dd26a6cc7a
Ryan Burda <ryanburda@gmail.com>
2024-09-30 08:22:54 -0400

    advanced git search setup

--- dotfiles/nvim/lua/plugins/init.lua
+++ dotfiles/nvim/lua/plugins/init.lua
@@ -160,28 +160,10 @@ return {
             },
             {
                 "aaronhallaert/advanced-git-search.nvim",
-                cmd = { "AdvancedGitSearch" },
-                config = function()
-                    -- optional: setup telescope before loading the extension
-                    require("telescope").setup{
-                        -- move this to the place where you call the telescope setup function
-                        extensions = {
-                            advanced_git_search = {
-                                    -- See Config
-                                }
-                        }
-                    }
-                    require("telescope").load_extension("advanced_git_search")
-                end,
                 dependencies = {
                     "nvim-telescope/telescope.nvim",
-                    -- to show diff splits and open commits in browser
                     "tpope/vim-fugitive",
-                    -- to open commits in browser with fugitive
-                    "tpope/vim-rhubarb",
-                    -- optional: to replace the diff from fugitive with diffview.nvim
-                    -- (fugitive is still needed to open in browser)
-                    -- "sindrets/diffview.nvim",
+                    "sindrets/diffview.nvim",
                 },
             },
         },
