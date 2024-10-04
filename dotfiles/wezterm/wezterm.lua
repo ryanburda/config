7624c0938a70563080fe116d24483790115fad5f
Ryan Burda <ryanburda@gmail.com>
2024-10-04 17:25:58 -0400

    removed helper function

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -34,19 +34,31 @@ config.window_padding = { left = 0, right = 0, top = 20, bottom = 0 }
 --     - <Image path>: The leaf of the image path relative to `background_image_dir` that will be shown.
 --          Similar to 'TRANSPARENT', the current colorschemes background will be rendered
 --          semi transparent to allow the image to show through.
+
+-- background image
 local current_background = helpers.get_var_from_file(background_file_path , "NONE")
 local background_image_path = nil
 
 if current_background ~= nil and current_background ~= "TRANSPARENT" and current_background ~= "NONE" then
     background_image_path =  background_image_dir .. current_background
+    -- Try opening the file to see if it exists
+    local file = io.open(background_file_path, "r")
+    if not file then
+        current_background = "NONE"
+        background_image_path = nil
+    else
+        file:close()
+    end
 end
 
+-- background color
 local background_color = nil
 
 if current_background ~= "NONE" then
     background_color = wezterm.get_builtin_color_schemes()[config.color_scheme].background
 end
 
+-- background opacity
 local is_nvim_background_dark = (helpers.get_var_from_file(nvim_background_file_path, 'dark')) == 'dark'
 local opacity
 
@@ -56,6 +68,37 @@ else
     opacity = 0.83
 end
 
-config.background = helpers.get_background_config(background_image_path, background_color, opacity)
+-- background config
+local background = {}
+
+if background_image_path ~= nil then
+    -- Lay the image down first.
+    table.insert(
+        background,
+        {
+            source = {
+                File = background_image_path
+            },
+        }
+    )
+end
+
+if background_color ~= nil then
+    -- Add the color layer on top.
+    table.insert(
+        background,
+        {
+            source = {
+                Color = background_color
+            },
+            opacity = opacity,
+            -- height and width needed due to https://github.com/wez/wezterm/issues/2817
+            height = '100%',
+            width = '100%',
+        }
+    )
+end
+
+config.background = background
 
 return config
