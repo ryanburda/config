acc5e672a49d6dcc03cb903aa2711117fdee85d8
Ryan Burda <ryanburda@gmail.com>
2024-10-15 09:04:24 -0400

    background image docs

--- dotfiles/scripts/macos.sh
+++ dotfiles/scripts/macos.sh
@@ -10,3 +10,12 @@ defaults write -g InitialKeyRepeat -int 15
 
 # Turn off CMD-Space shortcut for Spotlight search. (This is used by Alfred)
 /usr/libexec/PlistBuddy ~/Library/Preferences/com.apple.symbolichotkeys.plist -c "Set AppleSymbolicHotKeys:64:enabled false"
+
+cat <<EOF
+Manual Changes
+
+Battery -> Options -> Slightly dim the display on battery -> Off
+Displays -> Automatically adjust brightness -> Off
+Lock Screen -> Start Screen Saver when inactive -> Never
+Lock Screen -> Turn display off on battery when inactive -> For 5 minutes
+EOF
---
+++ dotfiles/wezterm/backgrounds/README.md
@@ -0,0 +1,5 @@
+Wezterm allocates a large amount of memory for high resolution images.
+It is a good idea to limit the size of these.
+```sh
+sips -Z 1080 *
+```
