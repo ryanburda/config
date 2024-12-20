a2eeb557c86bff7022c9419ed3d25a4777ceee1e
Ryan Burda <ryanburda@gmail.com>
2024-12-20 11:16:42 -0500

    s

--- dotfiles/nvim/lua/config/colorscheme.lua
+++ dotfiles/nvim/lua/config/colorscheme.lua
@@ -6,134 +6,134 @@ T.BACKGROUND_DEFAULT = 'dark'
 T.BACKGROUND_FILE = vim.fn.stdpath('config') .. '/.background'
 
 function T.set_colorscheme(is_setup)
-    is_setup = is_setup or false
+  is_setup = is_setup or false
 
-    -- create the colorscheme file if it doesn't already exist
-    if io.open(T.COLORSCHEME_FILE, "r") == nil then
-        local colorscheme_file = io.open(T.COLORSCHEME_FILE, "w+")
-        if colorscheme_file ~= nil then
-            colorscheme_file:write(T.COLORSCHEME_DEFAULT)
-            colorscheme_file:close()
-        else
-            print("Error: Could not open colorscheme file for writing.")
-        end
-    end
-
-    -- create the background file if it doesn't already exist
-    if io.open(T.BACKGROUND_FILE, "r") == nil then
-        local background_file = io.open(T.BACKGROUND_FILE, "w+")
-        if background_file ~= nil then
-            background_file:write(T.BACKGROUND_DEFAULT)
-            background_file:close()
-        else
-            print("Error: Could not open colorscheme file for writing.")
-        end
-    end
-
-    -- read the colorscheme from the colorscheme file
-    local colorscheme_file = io.open(T.COLORSCHEME_FILE, "r")
+  -- create the colorscheme file if it doesn't already exist
+  if io.open(T.COLORSCHEME_FILE, "r") == nil then
+    local colorscheme_file = io.open(T.COLORSCHEME_FILE, "w+")
     if colorscheme_file ~= nil then
-        local colorscheme = colorscheme_file:read("*a")
-        vim.cmd('colorscheme ' .. colorscheme)
-        colorscheme_file:close()
+      colorscheme_file:write(T.COLORSCHEME_DEFAULT)
+      colorscheme_file:close()
     else
-        print("Error: Could not read colorscheme file.")
+      print("Error: Could not open colorscheme file for writing.")
     end
+  end
 
-    -- read the background from the background file
-    local background_file = io.open(T.BACKGROUND_FILE, "r")
+  -- create the background file if it doesn't already exist
+  if io.open(T.BACKGROUND_FILE, "r") == nil then
+    local background_file = io.open(T.BACKGROUND_FILE, "w+")
     if background_file ~= nil then
-        local background = background_file:read("*a")
-        vim.cmd('set background=' .. background)
-        background_file:close()
+      background_file:write(T.BACKGROUND_DEFAULT)
+      background_file:close()
     else
-        print("Error: Could not read background file.")
+      print("Error: Could not open colorscheme file for writing.")
     end
+  end
 
-    -- Don't reload the colorizer plugin if neovim is just starting up.
-    if is_setup == false then
-        -- Reload colorizer plugin.
-        -- NOTE: must refresh the current buffer manually (`:e`)
-        vim.cmd('silent Lazy reload nvim-colorizer.lua')
-    end
+  -- read the colorscheme from the colorscheme file
+  local colorscheme_file = io.open(T.COLORSCHEME_FILE, "r")
+  if colorscheme_file ~= nil then
+    local colorscheme = colorscheme_file:read("*a")
+    vim.cmd('colorscheme ' .. colorscheme)
+    colorscheme_file:close()
+  else
+    print("Error: Could not read colorscheme file.")
+  end
+
+  -- read the background from the background file
+  local background_file = io.open(T.BACKGROUND_FILE, "r")
+  if background_file ~= nil then
+    local background = background_file:read("*a")
+    vim.cmd('set background=' .. background)
+    background_file:close()
+  else
+    print("Error: Could not read background file.")
+  end
+
+  -- Don't reload the colorizer plugin if neovim is just starting up.
+  if is_setup == false then
+    -- Reload colorizer plugin.
+    -- NOTE: must refresh the current buffer manually (`:e`)
+    vim.cmd('silent Lazy reload nvim-colorizer.lua')
+  end
 
 end
 
 local function get_var_from_file(file_path, default)
-    -- Reads value from `file_path`, returns `default` if file does not exist.
-    local file, _ = io.open(file_path, "r")
-    if not file then
-        -- Could not open file
-        return default
-    end
+  -- Reads value from `file_path`, returns `default` if file does not exist.
+  local file, _ = io.open(file_path, "r")
+  if not file then
+    -- Could not open file
+    return default
+  end
 
-    local content = file:read("*a")
+  local content = file:read("*a")
 
-    -- Remove trailing new lines
-    content = content:gsub("%s*$", "")
+  -- Remove trailing new lines
+  content = content:gsub("%s*$", "")
 
-    file:close()
-    return content
+  file:close()
+  return content
 end
 
 function T.set_transparent_background()
-    -- This function makes specific highlight groups transparent on a per colorscheme basis.
-    -- It is therefore important to make sure the terminal neovim is running in has a
-    -- background color that looks good with the current colorscheme.
-    --
-    -- If colors look bad you likely want to change the wezterm background colors.
-    -- If things are not transparent that should be then you likely want to add
-    -- additional highlight groups below.
+  -- This function makes specific highlight groups transparent on a per colorscheme basis.
+  -- It is therefore important to make sure the terminal neovim is running in has a
+  -- background color that looks good with the current colorscheme.
+  --
+  -- If colors look bad you likely want to change the wezterm background colors.
+  -- If things are not transparent that should be then you likely want to add
+  -- additional highlight groups below.
 
-    -- These highlight groups should be made transparent for all colorschemes.
-    vim.cmd("highlight Normal guibg=none ctermbg=none")
-    vim.cmd("highlight NormalNC guibg=none ctermbg=none")
-    vim.cmd("highlight NormalFloat guibg=none ctermbg=none")
-    vim.cmd("highlight NonText guibg=none ctermbg=none")
-    vim.cmd("highlight NeoTreeNormal guibg=none ctermbg=none")
-    vim.cmd("highlight NeoTreeNormalNC guibg=none ctermbg=none")
-    vim.cmd("highlight NeoTreeEndOfBuffer guibg=none ctermbg=none")
+  -- These highlight groups should be made transparent for all colorschemes.
+  vim.cmd("highlight Normal guibg=none ctermbg=none")
+  vim.cmd("highlight NormalNC guibg=none ctermbg=none")
+  vim.cmd("highlight NormalFloat guibg=none ctermbg=none")
+  vim.cmd("highlight NonText guibg=none ctermbg=none")
+  vim.cmd("highlight NeoTreeNormal guibg=none ctermbg=none")
+  vim.cmd("highlight NeoTreeNormalNC guibg=none ctermbg=none")
+  vim.cmd("highlight NeoTreeEndOfBuffer guibg=none ctermbg=none")
 
-    -- Colorscheme specific highlight group changes
-    -- local colorscheme = vim.g.colors_name
-    -- if colorscheme == "adwaita" then
-    --     vim.cmd("highlight NeoTreeNormal guibg=none ctermbg=none")
-    --     vim.cmd("highlight NeoTreeNormalNC guibg=none ctermbg=none")
-    --     vim.cmd("highlight NeoTreeEndOfBuffer guibg=none ctermbg=none")
-    -- elseif colorscheme == "gruvbox-material" then
-    --     vim.cmd("highlight NeoTreeNormal guibg=none ctermbg=none")
-    --     vim.cmd("highlight NeoTreeNormalNC guibg=none ctermbg=none")
-    --     vim.cmd("highlight NeoTreeEndOfBuffer guibg=none ctermbg=none")
-    -- end
+  -- Colorscheme specific highlight group changes
+  -- local colorscheme = vim.g.colors_name
+  -- if colorscheme == "adwaita" then
+  --     vim.cmd("highlight NeoTreeNormal guibg=none ctermbg=none")
+  --     vim.cmd("highlight NeoTreeNormalNC guibg=none ctermbg=none")
+  --     vim.cmd("highlight NeoTreeEndOfBuffer guibg=none ctermbg=none")
+  -- elseif colorscheme == "gruvbox-material" then
+  --     vim.cmd("highlight NeoTreeNormal guibg=none ctermbg=none")
+  --     vim.cmd("highlight NeoTreeNormalNC guibg=none ctermbg=none")
+  --     vim.cmd("highlight NeoTreeEndOfBuffer guibg=none ctermbg=none")
+  -- end
 end
 
 function T.set_transparent_background_conditional()
-    -- Read the .background file
-    local background = get_var_from_file(os.getenv("HOME") .. "/.config/wezterm/.background", "NONE")
+  -- Read the .background file
+  local background = get_var_from_file(os.getenv("HOME") .. "/.config/wezterm/.background", "NONE")
 
-    if background ~= "NONE" then
-        T.set_transparent_background()
-    end
+  if background ~= "NONE" then
+    T.set_transparent_background()
+  end
 end
 
 function T.setup()
-    -- Autocommand to make specific highlight groups transparent when an background image is being shown.
-    -- This will run automatically whenever the colorscheme changes.
-    vim.api.nvim_create_autocmd("ColorScheme", {
-        pattern = "*",
-        callback = T.set_transparent_background_conditional
-    })
+  -- Autocommand to make specific highlight groups transparent when an background image is being shown.
+  -- This will run automatically whenever the colorscheme changes.
+  vim.api.nvim_create_autocmd("ColorScheme", {
+    pattern = "*",
+    callback = T.set_transparent_background_conditional
+  })
 
-    T.set_colorscheme(true)
+  T.set_colorscheme(true)
 
-    -- update the colorscheme when the colorscheme file changes.
-    local fwatch = require('fwatch')
-    fwatch.watch(T.COLORSCHEME_FILE, {
-        on_event = function()
-            -- sleep for a bit to make sure the background file is done being written to as well.
-            vim.schedule(function() vim.defer_fn(T.set_colorscheme, 100) end)
-        end
-    })
+  -- update the colorscheme when the colorscheme file changes.
+  local fwatch = require('fwatch')
+  fwatch.watch(T.COLORSCHEME_FILE, {
+    on_event = function()
+      -- sleep for a bit to make sure the background file is done being written to as well.
+      vim.schedule(function() vim.defer_fn(T.set_colorscheme, 100) end)
+    end
+  })
 
 end
 
--- dotfiles/nvim/lua/config/telescope.lua
+++ dotfiles/nvim/lua/config/telescope.lua
@@ -2,122 +2,122 @@ local T = {}
 
 -- setup
 function T.getqflist_files()
-    local qfl = vim.fn.getqflist()
-    local files = {}
+  local qfl = vim.fn.getqflist()
+  local files = {}
 
-    for _,v in pairs(qfl) do
-        table.insert(files, v.text)
-    end
+  for _,v in pairs(qfl) do
+    table.insert(files, v.text)
+  end
 
-    return files
+  return files
 end
 
 function T.setup()
 
-    local builtin = require("telescope.builtin")
-    local sorters = require("telescope.sorters")
-    local actions = require("telescope.actions")
-    local previewers = require("telescope.previewers")
-    local cf_actions = require('telescope').extensions.changed_files.actions
-    local lga_actions = require("telescope-live-grep-args.actions")
+  local builtin = require("telescope.builtin")
+  local sorters = require("telescope.sorters")
+  local actions = require("telescope.actions")
+  local previewers = require("telescope.previewers")
+  local cf_actions = require('telescope').extensions.changed_files.actions
+  local lga_actions = require("telescope-live-grep-args.actions")
 
-    require("telescope").setup({
-        defaults = {
-            file_sorter = sorters.get_fzy_sorter,
-            color_devicons = true,
-            file_previewer = previewers.vim_buffer_cat.new,
-            grep_previewer = previewers.vim_buffer_vimgrep.new,
-            qflist_previewer = previewers.vim_buffer_qflist.new,
-            mappings = {
-                i = {
-                    ["<C-x>"] = false,
-                    ["<C-y>"] = function()
-                        local entry = require("telescope.actions.state").get_selected_entry()
-                        local cb_opts = vim.opt.clipboard:get()
-                        if vim.tbl_contains(cb_opts, "unnamed") then vim.fn.setreg("*", entry.path) end
-                        if vim.tbl_contains(cb_opts, "unnamedplus") then
-                          vim.fn.setreg("+", entry.path)
-                        end
-                        vim.fn.setreg("", entry.path)
-                    end,
-                    ["<C-k>"] = actions.send_to_qflist,
-                    ["<C-o>"] = function(prompt_bufnr)
-                        -- Open buffer and resume prompt
-                        actions.select_default(prompt_bufnr)
-                        builtin.resume()
-                    end,
-                },
-            },
+  require("telescope").setup({
+    defaults = {
+      file_sorter = sorters.get_fzy_sorter,
+      color_devicons = true,
+      file_previewer = previewers.vim_buffer_cat.new,
+      grep_previewer = previewers.vim_buffer_vimgrep.new,
+      qflist_previewer = previewers.vim_buffer_qflist.new,
+      mappings = {
+        i = {
+          ["<C-x>"] = false,
+          ["<C-y>"] = function()
+            local entry = require("telescope.actions.state").get_selected_entry()
+            local cb_opts = vim.opt.clipboard:get()
+            if vim.tbl_contains(cb_opts, "unnamed") then vim.fn.setreg("*", entry.path) end
+            if vim.tbl_contains(cb_opts, "unnamedplus") then
+             vim.fn.setreg("+", entry.path)
+            end
+            vim.fn.setreg("", entry.path)
+          end,
+          ["<C-k>"] = actions.send_to_qflist,
+          ["<C-o>"] = function(prompt_bufnr)
+            -- Open buffer and resume prompt
+            actions.select_default(prompt_bufnr)
+            builtin.resume()
+          end,
         },
-        extensions = {
-            fzf = {
-                fuzzy = true,
-                override_generic_sorter = true,
-                override_file_sorter = true,
-                case_mode = "smart_case",
-            },
-            live_grep_args = {
-                auto_quoting = true, -- enable/disable auto-quoting
-                -- define mappings, e.g.
-                mappings = { -- extend mappings
-                    i = {
-                        ["<C-f>"] = lga_actions.quote_prompt({ postfix = " --iglob **/" }),
-                    },
-                },
-            },
-            advanced_git_search = {
-                diff_plugin = "diffview"
-            },
+      },
+    },
+    extensions = {
+      fzf = {
+        fuzzy = true,
+        override_generic_sorter = true,
+        override_file_sorter = true,
+        case_mode = "smart_case",
+      },
+      live_grep_args = {
+        auto_quoting = true, -- enable/disable auto-quoting
+        -- define mappings, e.g.
+        mappings = { -- extend mappings
+          i = {
+            ["<C-f>"] = lga_actions.quote_prompt({ postfix = " --iglob **/" }),
+          },
         },
-        pickers = {
-            find_files = {
-                cwd = vim.fn.getcwd(),
-                results_title = vim.fn.getcwd(),
-                find_command = { "rg", "--ignore", "--follow", "--files", "--hidden", "-g", "!.git/" },
-            },
-            live_grep = {
-                cwd = vim.fn.getcwd(),
-                results_title = vim.fn.getcwd(),
-                find_command = { "rg", "--ignore", "-L", "--files", "--hidden", "-g", "!.git/" },
-            },
-            git_branches = {
-                mappings = {
-                    i = {
-                        ["<C-o>"] = actions.git_switch_branch,
-                        ["<C-l>"] = cf_actions.find_changed_files,
-                        ["<CR>"] = function(prompt_bufnr)
-                            -- get the selected branch name
-                            local entry = require("telescope.actions.state").get_selected_entry()
-                            -- close telescope
-                            require("telescope.actions").close(prompt_bufnr)
-                            -- open diffview
-                            vim.cmd('DiffviewOpen ' .. entry.name)
-                        end,
-                    }
-                }
-            },
-            git_commits = {
-                mappings = {
-                    i = {
-                        ["<C-o>"] = actions.git_checkout,
-                        ["<C-l>"] = cf_actions.find_changed_files,
-                        ["<CR>"] = function(prompt_bufnr)
-                            -- get the selected commit hash
-                            local entry = require("telescope.actions.state").get_selected_entry()
-                            -- close telescope
-                            require("telescope.actions").close(prompt_bufnr)
-                            -- open diffview
-                            vim.cmd('DiffviewOpen ' .. entry.value)
-                        end,
-                    }
-                }
-            },
-        },
-    })
+      },
+      advanced_git_search = {
+        diff_plugin = "diffview"
+      },
+    },
+    pickers = {
+      find_files = {
+        cwd = vim.fn.getcwd(),
+        results_title = vim.fn.getcwd(),
+        find_command = { "rg", "--ignore", "--follow", "--files", "--hidden", "-g", "!.git/" },
+      },
+      live_grep = {
+        cwd = vim.fn.getcwd(),
+        results_title = vim.fn.getcwd(),
+        find_command = { "rg", "--ignore", "-L", "--files", "--hidden", "-g", "!.git/" },
+      },
+      git_branches = {
+        mappings = {
+          i = {
+            ["<C-o>"] = actions.git_switch_branch,
+            ["<C-l>"] = cf_actions.find_changed_files,
+            ["<CR>"] = function(prompt_bufnr)
+              -- get the selected branch name
+              local entry = require("telescope.actions.state").get_selected_entry()
+              -- close telescope
+              require("telescope.actions").close(prompt_bufnr)
+              -- open diffview
+              vim.cmd('DiffviewOpen ' .. entry.name)
+            end,
+          }
+        }
+      },
+      git_commits = {
+        mappings = {
+          i = {
+            ["<C-o>"] = actions.git_checkout,
+            ["<C-l>"] = cf_actions.find_changed_files,
+            ["<CR>"] = function(prompt_bufnr)
+              -- get the selected commit hash
+              local entry = require("telescope.actions.state").get_selected_entry()
+              -- close telescope
+              require("telescope.actions").close(prompt_bufnr)
+              -- open diffview
+              vim.cmd('DiffviewOpen ' .. entry.value)
+            end,
+          }
+        }
+      },
+    },
+  })
 
-    require('telescope').load_extension('fzf')
-    require("telescope").load_extension("changed_files")
-    require("telescope").load_extension("advanced_git_search")
+  require('telescope').load_extension('fzf')
+  require("telescope").load_extension("changed_files")
+  require("telescope").load_extension("advanced_git_search")
 
 end
 
