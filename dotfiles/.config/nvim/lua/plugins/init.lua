1cf8e199fa5ad08a82e67386920ce9edd48958f4
Ryan Burda <ryanburda@gmail.com>
2025-03-20 14:11:38 -0400

    remove indent-blankline

--- dotfiles/.config/nvim/lua/plugins/init.lua
+++ dotfiles/.config/nvim/lua/plugins/init.lua
@@ -254,18 +254,6 @@ return {
     end
   },
 
-  {
-    'lukas-reineke/indent-blankline.nvim',
-    config = function ()
-      require("ibl").setup {
-        scope = {
-          show_start = false,
-          show_end = false
-        }
-      }
-    end
-  },
-
   ----------------------------
   -- Package Manager: Mason --
   ----------------------------
