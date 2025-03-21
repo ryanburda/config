a10f34cd48ff961cfc938cf13bc1c2657c152620
Ryan Burda <ryanburda@gmail.com>
2025-03-21 14:07:22 -0400

    remove nvim-tree-context

--- dotfiles/.config/nvim/lua/plugins/init.lua
+++ dotfiles/.config/nvim/lua/plugins/init.lua
@@ -68,25 +68,6 @@ return {
       })
     end
   },
-  {
-    'nvim-treesitter/nvim-treesitter-context',
-    config = function ()
-      require('treesitter-context').setup({
-        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-        max_lines = 4, -- How many lines the window should span. Values <= 0 mean no limit.
-        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-        line_numbers = true,
-        multiline_threshold = 20, -- Maximum number of lines to show for a single context
-        trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-        mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
-        -- Separator between context and content. Should be a single character string, like '-'.
-        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-        separator = nil,
-        zindex = 20, -- The Z-index of the context window
-        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-      })
-    end
-  },
   -- autoformat on save
   {
     'stevearc/conform.nvim',
@@ -236,6 +217,7 @@ return {
   ----------------------------
   {
     "williamboman/mason.nvim",
+    lazy = true,
     config = function ()
       require("mason").setup()
     end
@@ -285,13 +267,7 @@ return {
   },
 
   -- Debug
-  {
-    'mfussenegger/nvim-dap',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-    },
-    lazy = true,
-  },
+  'mfussenegger/nvim-dap',
   {
     'rcarriga/nvim-dap-ui',
     dependencies = {
