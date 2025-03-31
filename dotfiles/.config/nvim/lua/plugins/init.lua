d9c9d79de7e216d9b7f2c04453aad7116b2b04a6
Ryan Burda <ryanburda@gmail.com>
2025-03-31 08:43:35 -0400

    simplified nvim-treesitter config

--- dotfiles/.config/nvim/lua/plugins/init.lua
+++ dotfiles/.config/nvim/lua/plugins/init.lua
@@ -30,44 +30,17 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     config = function ()
+      ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup({
-        ensure_installed = {
-          "c",
-          "go",
-          "lua",
-          "python",
-          "query",
-          "rust",
-          "sql",
-          "vim",
-          "vimdoc",
-        },
-        sync_install = false,
         auto_install = true,
-        ignore_install = {},
         highlight = {
           enable = true,
-          -- disable treesitter highlight for large files
-          disable = function(_, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-              return true
-            end
-          end,
           additional_vim_regex_highlighting = false,
-        },
-        indent = { enable = true },
-        incremental_selection = {
-          enable = true,
-          keymaps =  {
-            node_incremental = '<C-n>',
-            node_decremental = '<C-p>',
-          },
         },
       })
     end
   },
+
   -- autoformat on save
   {
     'stevearc/conform.nvim',
