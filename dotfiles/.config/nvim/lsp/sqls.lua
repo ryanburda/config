ce929267a2545a6d0244b5c795998bc4ce56a69e
Ryan Burda <ryanburda@gmail.com>
2025-05-27 13:05:07 -0400

    docker terraform ls

---
+++ dotfiles/.config/nvim/lsp/sqls.lua
@@ -0,0 +1,7 @@
+-- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/sqls.lua
+return {
+  cmd = { 'sqls' },
+  filetypes = { 'sql', 'mysql' },
+  root_markers = { 'config.yml' },
+  settings = {},
+}
