66ec485d79f3351f92356648444828759db1d69f
Ryan Burda <ryanburda@gmail.com>
2025-11-30 15:14:43 -0500

    colorscheme overrides

--- dotfiles/.config/nvim/lua/colorscheme.lua
+++ dotfiles/.config/nvim/lua/colorscheme.lua
@@ -5,10 +5,21 @@ Sets the colorscheme based on environment variables
 --]]
 local env = require('env')
 
--- Callback function triggered on file change
-vim.cmd('colorscheme ' .. env.envget('nvim_colorscheme', 'everforest'))
-vim.cmd('set background=' .. env.envget('nvim_background', 'dark'))
+local colorscheme = env.envget('nvim_colorscheme', 'everforest')
+local background = env.envget('nvim_background', 'dark')
 
+vim.cmd('colorscheme ' .. colorscheme)
+vim.cmd('set background=' .. background)
+
+-- Custom colorscheme overrides
+-- if colorscheme == 'everforest' and background == 'dark' then
+--   vim.cmd("highlight StatusLine guibg=#3a3a3a ctermbg=234")
+--   vim.cmd("highlight StatusLineNC guibg=#3a3a3a ctermbg=234")
+--   vim.cmd("highlight SignColumn guibg=#3a3a3a ctermbg=234")
+--   vim.cmd("highlight lualine_c_normal guibg=#3a3a3a ctermbg=234")
+-- end
+
+-- Transparency
 if env.envget('wezterm_background', 'NONE') ~= "NONE" then
   -- These highlight groups should be made transparent when displaying a background.
   vim.cmd("highlight Normal guibg=none ctermbg=none")
