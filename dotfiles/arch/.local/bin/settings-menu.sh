7804eaa0263f669ba76f6c6eb1c0d297c6b70907
Ryan Burda <ryanburda@gmail.com>
2026-02-10 13:41:11 -0500

    monitor

--- dotfiles/arch/.local/bin/settings-menu.sh
+++ dotfiles/arch/.local/bin/settings-menu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-option=$(printf "sound\nwifi\nbluetooth\nmonitor" | fuzzel --dmenu --prompt "")
+option=$(printf "sound\nwifi\nbluetooth" | fuzzel --dmenu --prompt "")
 
 case "$option" in
     sound)
@@ -11,8 +11,5 @@ case "$option" in
         ;;
     bluetooth)
         wezterm start -- bluetui
-        ;;
-    monitor)
-        wezterm start -- btm --rate 5s
         ;;
 esac
