49b80961a78929f05b9482b3532504b828ad3801
Ryan Burda <ryanburda@gmail.com>
2026-03-07 19:48:56 -0500

    wrap keybinds

--- dotfiles/common/.config/nvim/lua/keymaps.lua
+++ dotfiles/common/.config/nvim/lua/keymaps.lua
@@ -68,6 +68,12 @@ vim.keymap.set(
 ------------------------------------------------------------------------------------------------------------------------
 -- Navigation
 ------------------------------------------------------------------------------------------------------------------------
+-- Helpful when text wraps.
+vim.keymap.set('n', 'k', 'gk', {desc = 'Navigation: Up one display line'})
+vim.keymap.set('n', 'j', 'gj', {desc = 'Navigation: Down one display line'})
+vim.keymap.set('n', '0', 'g0', {desc = 'Navigation: Beginning of display line'})
+vim.keymap.set('n', '$', 'g$', {desc = 'Navigation: End of display line'})
+
 vim.keymap.set(
   'n',
   '<C-d>',
