22297874df4f9844e7405344354a65f0b6ac12e8
Ryan Burda <ryanburda@gmail.com>
2026-09-15 09:42:33 -0400

    nightfox-night

--- dotfiles/common/.config/wezterm/colorschemes.lua
+++ dotfiles/common/.config/wezterm/colorschemes.lua
@@ -154,8 +154,8 @@ return {
     selection_bg = "#2b3b51",
     selection_fg = "#cdcecf",
     split = "#131a24",
-    ansi = { "#393b44", "#c94f6d", "#81b29a", "#dbc074", "#719cd6", "#9d79d6", "#63cdcf", "#dfdfe0" },
-    brights = { "#575860", "#d16983", "#8ebaa4", "#e0c989", "#86abdc", "#baa1e2", "#7ad5d6", "#e4e4e5" },
+    ansi = { "#131a24", "#c94f6d", "#81b29a", "#dbc074", "#719cd6", "#9d79d6", "#63cdcf", "#dfdfe0" },
+    brights = { "#29394f", "#d16983", "#8ebaa4", "#e0c989", "#86abdc", "#baa1e2", "#7ad5d6", "#e4e4e5" },
     tab_bar = {
       background = "#131a24",
       active_tab = { bg_color = "#71839b", fg_color = "#192330" },
