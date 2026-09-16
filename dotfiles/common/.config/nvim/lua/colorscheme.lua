00b6a3b2303b47055d306bc9ffc0d12dde4b8259
Ryan Burda <ryanburda@gmail.com>
2026-09-16 19:25:25 -0400

    transparency toggle (remove backgrounds)

--- dotfiles/common/.config/nvim/lua/colorscheme.lua
+++ dotfiles/common/.config/nvim/lua/colorscheme.lua
@@ -12,8 +12,8 @@ vim.cmd('colorscheme ' .. colorscheme)
 vim.cmd('set background=' .. background)
 
 -- Transparency
-if envy.get('wezterm_background', 'NONE') ~= "NONE" then
-  -- These highlight groups should be made transparent when displaying a background.
+if envy.get('wezterm_transparency', 'false') == 'true' then
+  -- These highlight groups should be made transparent when the terminal is.
   vim.cmd("highlight Normal guibg=none ctermbg=none")
   vim.cmd("highlight NormalNC guibg=none ctermbg=none")
   vim.cmd("highlight NormalFloat guibg=none ctermbg=none")
--- dotfiles/common/.config/wezterm/wezterm.lua
+++ dotfiles/common/.config/wezterm/wezterm.lua
@@ -6,10 +6,6 @@ local wezterm = require("wezterm")
 local colorschemes = require("colorschemes")
 local config = wezterm.config_builder()
 
--- important paths
--- NOTE: `os.getenv("XDG_CONFIG_HOME")` returns nil. Using "HOME" as an alternative for now.
-local background_image_dir = os.getenv("HOME") .. "/code/assets/base/assets/backgrounds/"
-
 -- COLORSCHEME
 --
 -- The terminal palette is keyed off the neovim colorscheme rather than carrying
@@ -51,69 +47,26 @@ config.warn_about_missing_glyphs = false
 
 -- BACKGROUND
 --
--- the wezterm_background environment variable can be one of the following:
---   - 'NONE': if no background image or color layer should be shown. (default background)
---   - 'TRANSPARENT': the current colorschemes background will be rendered semi transparent
---       to allow any windows under the terminal to show through.
---   - <Image path>: The leaf of the image path relative to `background_image_dir` that will be shown.
---       Similar to 'TRANSPARENT', the current colorschemes background will be rendered
---       semi transparent to allow the image to show through.
+-- `wezterm_transparency` is either 'true' or 'false'. When true the current
+-- colorscheme's background is rendered semi transparent so that any windows
+-- under the terminal show through.
+if envy.get('wezterm_transparency', 'false') == 'true' then
+  local opacity = 0.96
+  if nvim_background ~= 'dark' then
+    opacity = 0.92
+  end
 
--- background image
-local current_background = envy.get("wezterm_background", "NONE")
-local background_image_path = nil
-if current_background ~= "NONE" and current_background ~= "TRANSPARENT" then
-  background_image_path = background_image_dir .. current_background
-end
-
--- background color
-local background_color = nil
-
-if current_background ~= "NONE" then
-  background_color = scheme.background
-end
-
--- background opacity
-local is_nvim_background_dark = nvim_background == 'dark'
-local opacity
-
-if is_nvim_background_dark then
-  opacity = 0.96
-else
-  opacity = 0.92
-end
-
--- background config
-local background = {}
-
-if background_image_path ~= nil then
-  -- Lay the image down first.
-  table.insert(
-    background,
+  config.background = {
     {
       source = {
-        File = background_image_path
-      },
-    }
-  )
-end
-
-if background_color ~= nil then
-  -- Add the color layer on top.
-  table.insert(
-    background,
-    {
-      source = {
-        Color = background_color
+        Color = scheme.background
       },
       opacity = opacity,
       -- height and width needed due to https://github.com/wez/wezterm/issues/2817
       height = '100%',
       width = '100%',
     }
-  )
+  }
 end
-
-config.background = background
 
 return config
